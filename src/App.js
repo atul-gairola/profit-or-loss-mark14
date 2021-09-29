551600b005a63cd4c8e5a4d45b30f0c5ba326678
atul-gairola <atulgairola08@gmail.com>
2021-09-29 06:12:41 +0530

    feat: logic for profit and loss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,49 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    initial: "",
+    quantity: "",
+    current: "",
+  });
+  const [result, setResult] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setResult(null);
+    const { initial, quantity, current } = data;
+
+    if (initial && quantity && current) {
+      const initialAmount = initial * quantity;
+      const currentAmount = current * quantity;
+
+      if (currentAmount > initialAmount) {
+        const profit = currentAmount - initialAmount;
+        const percentage = (profit / initialAmount) * 100;
+        setResult(
+          `This Stock Is In PROFIT of  ₹${profit}, ${percentage.toFixed(2)}% ⬆`
+        );
+      } else if (currentAmount === initialAmount) {
+        setResult("This Stock Is Neither In Profit Nor In Loss ");
+      } else {
+        const loss = initialAmount - currentAmount;
+        const percentage = (loss / initialAmount) * 100;
+        setResult(
+          `This Stock Is In LOSS of  ₹${loss}, ${percentage.toFixed(2)}% ⬇`
+        );
+      }
+    }
+  };
+
   return (
     <div className="App">
       <header>
@@ -9,13 +52,14 @@ function App() {
       <main>
         <form>
           <label htmlFor="initial">Initial Price</label>
-          <input type="number" name="initial" />
-          <label htmlFor="initial">Quantity Stocks</label>
-          <input type="number" name="quantity" />
-          <label htmlFor="initial">Current Price</label>
-          <input type="number" name="current" />
-          <button>Submit</button>
+          <input type="number" name="initial" onChange={handleInputChange} />
+          <label htmlFor="quantity">Quantity Stocks</label>
+          <input type="number" name="quantity" onChange={handleInputChange} />
+          <label htmlFor="current">Current Price</label>
+          <input type="number" name="current" onChange={handleInputChange} />
+          <button onClick={handleSubmit}>Submit</button>
         </form>
+        {result !== null && <p>{result}</p>}
       </main>
       <footer>
         Created with love by <a href="https://atulgairola.tech">Atul Gairola</a>
